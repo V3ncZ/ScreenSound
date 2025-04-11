e729080f662af5b70dc4e4ecfaa344817b685293
VenCz <viniciuscesarp3@gmail.com>
2025-04-10 21:12:50 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Banco/DAL.cs
@@ -0,0 +1,49 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco
+{
+    // Representação de uma classe abstrata que implementa um generics, onde o T deve ser uma classe
+    internal class DAL<T> where T : class
+    {
+
+        protected readonly ScreenSoundContext _context;
+
+        public DAL(ScreenSoundContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<T> Listar()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Adicionar(T objeto)
+        {
+            _context.Set<T>().Add(objeto);
+            _context.SaveChanges();
+        }
+
+        public void Deletar(T objeto)
+        {
+            _context.Set<T>().Remove(objeto);
+            _context.SaveChanges();
+        }
+
+        public void Atualizar(T objeto)
+        {
+            _context.Set<T>().Update(objeto);
+            _context.SaveChanges();
+        }
+
+        public T? RecuperarPor(Func<T, bool> condicao)
+        {
+            return _context.Set<T>().FirstOrDefault(condicao);
+        }
+
+        public IEnumerable<T> ListarPor(Func<T, bool> condicao)
+        {
+            return _context.Set<T>().Where(condicao);
+        }
+    }
+}
---
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco;
+
+internal class ScreenSoundContext : DbContext
+{
+    // Mapeando a entidade Artista da classe para a tabela Artistas no banco de dados
+    public DbSet<Artista> Artistas { get; set; }
+
+    // Mapeando a entidade Musica da classe para a tabela Musicas no banco de dados
+    public DbSet<Musica> Musicas { get; set; }
+
+
+    // Conexao com o banco de dados
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundv0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    // Metodo para obter a conexao com o banco de dados utilizando entity
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString)
+            .UseLazyLoadingProxies();
+    }
+
+    
+    
+
+}
